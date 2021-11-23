beb524851b64e16a8357fcbf29f9bd9a668fcb9e
dominggo1999 <arnolddominggo19@gmail.com>
2021-11-23 21:35:36 +0700

    boilerplate

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default{
+  data() {
+    return {
+      name: 'Welcome To Vue',
+    };
+  },
+};
+
+
+</script>
+
+<template>
+  <div class="flex w-full h-screen bg-green-100 items-center justify-center">
+    <p class="text-green-500 font-bold text-4xl">{{name}}</p>
+  </div>
+</template>
+
+<style>
+</style>
